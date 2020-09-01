6bad0e3053baa3c5e55d98faf92dc734bb8e00cc
tinali0 <xzl5423@psu.edu>
2020-09-01 14:15:05 +0000

    Initial commit

---
+++ main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(void) {
+  double temp;
+  printf("Enter temperature in celsius: \n");
+  scanf("%lf", &temp);
+  printf("%f° in Celsius is equivalent to %f°Fahrenheit. \n", temp,temp*9/5+32);
+  return 0;
+}

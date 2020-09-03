f7ad3889937c6fddc0953074d3adec8ade10c826
tinali0 <xzl5423@psu.edu>
2020-09-03 01:32:01 +0000

    author

--- main.c
+++ main.c
@@ -1,16 +1,16 @@
+// Author: Xiangting Li xzl5423@psu.edu
 #include <stdio.h>
 #include <readline/readline.h>
-#include<stdlib.h>
-int main(void) {
-  double fahrenheit;
+#include <stdlib.h>
+double fahrenheit;
+
+int main(void) { 
+
   char *celsius= readline("Enter temperature in celsius: ");
 
   fahrenheit = (1.8 * atof(celsius) + 32.0);
   
   printf("%f° in Celsius is equivalent to %f° Fahrenheit.\n",atof(celsius), fahrenheit);
-
-
- 
   
   return 0;
 }
